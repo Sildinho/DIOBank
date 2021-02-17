459eb8ff1da0b45be0e5235d74d2d02d2a8f4d05
Sildinho Junior <sildojunior@gmail.com>
2021-02-17 14:24:09 -0300

    Adicionar arquivos de projeto.

---
+++ TransferenciasBancariasDIO/Enum/TipoConta.cs
@@ -0,0 +1,9 @@
+﻿// criado seguindo os passos do curso "Criando uma aplicação de transferências bancárias com .NET - Eliézer Zarpelão" - DIO
+namespace TransferenciasBancariasDIO // pasta retirada
+{
+    public enum TipoConta
+    {
+        PessoaFisica = 1,
+        PessoaJuridica = 2
+    }
+}
